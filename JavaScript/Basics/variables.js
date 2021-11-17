b1b155514cb7fdabd9635b68ea61af5cbf2773f3
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-16 21:28:37 -0500

    variable

---
+++ JavaScript/Basics/variables.js
@@ -0,0 +1,8 @@
+let petName = 'Biggie'
+
+// Okay to reassign variable 
+petName = 'Bella'
+
+// There are rules related to variable names
+// variable name has to start with a letter cannot start with a number
+// variable name cannot be reserved with key word like let
