753c4318a2a21101ed8af581b665b1f33c511084
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-16 21:20:50 -0500

    scope

---
+++ JavaScript/Basics/scope.js
@@ -0,0 +1,13 @@
+let name = 'Sierra'
+
+if (true) {
+    // let Name = 'Nikki
+    if (true) {
+        let name = 'Nikola'
+        console.log(name)
+    }
+}
+
+if (true) {
+    console.log(name)
+}
