766769fc1751f3b7beccf5a66c136040a69affd5
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-16 21:18:03 -0500

    numbers challenge

---
+++ JavaScript/Basics/Numbers_Challenge.js
@@ -0,0 +1,11 @@
+// studentScore 18
+let studentScore = 18
+
+// maxScore 20
+let maxScore = 20
+
+// percent 90
+ let percent = (studentScore/maxScore)*100
+
+ // print the percent
+ console.log(percent)
