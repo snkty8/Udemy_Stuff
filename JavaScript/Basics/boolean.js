c342b26f15dd7f2b8f1d1457705e363a84c8a8d7
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-16 21:03:56 -0500

    boolean

---
+++ JavaScript/Basics/boolean.js
@@ -0,0 +1,30 @@
+// === is equal to
+// !== is not equal
+// < less than 
+// > greater than 
+
+let temp = 123
+
+if (temp <= 32) {
+    console.log('It is freezing outside')
+}
+
+if (temp >= 100) {
+    console.log('It is way too hot outside')
+    console.log('Testing123')
+}
+
+// Challenge Area
+
+// create age variable
+let age = 5
+
+// if 7 or under print message
+if (age <=7) {
+    console.log('You can get free candy')
+}
+
+// if 65 or older print message
+if (age >= 65) {
+    console.log('You can get a 20 percent discount')
+}
