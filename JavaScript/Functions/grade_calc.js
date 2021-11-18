19d6a5b656567dda2bef02cf7d78678b4e56e610
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-17 22:00:30 -0500

    function challenge

---
+++ JavaScript/Functions/grade_calc.js
@@ -0,0 +1,23 @@
+// students score and total possible score
+// 15/20 You got a C (75%)!
+
+
+let grade_cal = function (score, possibleScore) {
+    percent = (score/possibleScore)*100
+    if (percent >= 90) {
+        letterGrade = 'A'
+    } else if (percent <=89 && percent >= 80) {
+        letterGrade = 'B'
+    } else if (percent <= 79 && percent>=70) {
+        letterGrade = 'C'
+    } else if (percent <= 69 && percent >= 60) {
+        letterGrade = 'D'
+    } else {
+        letterGrade = 'F'
+    }
+    
+    return `Your score is ${score} out of a possible ${possibleScore}. That is ${percent}% You got a ${letterGrade}!!`
+}
+
+let grade = grade_cal(75, 100)
+console.log(grade)
