5af2b97e5fcb1fb52408ad7342c650bc3be24151
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-17 21:17:54 -0500

    multiple arguments

---
+++ JavaScript/Functions/arguments.js
@@ -0,0 +1,26 @@
+// Function with multiple arguments
+let add = function (a, b, c) {
+    return a + b + c
+}
+
+let result = add(10, 1, 5)
+console.log(result)
+
+// Default Arguments
+let getScoreText = function (Name = 'Anonymous', score = 0) {
+    return 'Name: ' + Name + ' - Score: ' + score
+}
+
+let scoreText = getScoreText(undefined, 99)
+console.log(scoreText)
+
+// Challenge Area
+
+// function take two arguments: total bill and tipPercent 
+let getTip = function (bill = 0, tipPercent = .2) {
+    totalBill = bill + (bill * tipPercent)
+    return 'Total Bill: ' + totalBill
+}
+
+let grandTotal = getTip(10)
+console.log(grandTotal)
