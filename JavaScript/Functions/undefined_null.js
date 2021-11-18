866edad8377f9c263c051e621cc251f5f809e888
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-17 20:55:56 -0500

    undefined and null

---
+++ JavaScript/Functions/undefined_null.js
@@ -0,0 +1,27 @@
+// Undefined for variable
+
+let Name  = 'Jen'
+
+if (Name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log(Name)
+}
+
+
+// Undefined for function arguments
+// Undefined as function return default value
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square()
+console.log(result) 
+
+// Null as assigned value, undefined is a default in JS
+let age = 27
+
+age = null
+
+console.log(age)
+
