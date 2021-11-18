a8793bf9df0853f57ab9460905983937b611ebef
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-17 21:25:33 -0500

    function scope

---
+++ JavaScript/Functions/function_scope.js
@@ -0,0 +1,22 @@
+// Global scope (convert farenhiet to celccius, valueTwo, valueThree)
+    // Local Scope (F, C)
+        // Local scope (isFreezing)
+
+
+let FarenheitToCelcius = function(F) {
+    let C = (F-32)*(5/9)
+
+    if (C <= 0) {
+        let isFreezing = true
+    }
+
+    return C
+}
+
+let valueTwo = FarenheitToCelcius(68)
+let valueThree = FarenheitToCelcius(20)
+
+console.log(valueTwo)
+console.log(valueThree)
+
+
