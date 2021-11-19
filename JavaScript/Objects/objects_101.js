8e5e8a63eb333e2bbdade646c5222a2df7fe20fb
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-19 13:19:51 -0500

    objects 2

---
+++ JavaScript/Objects/objects_101.js
@@ -0,0 +1,30 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// Change the title of the book
+myBook.title = 'Animal Farm'
+console.log(`${myBook.title} by ${myBook.author}`)
+
+
+// Challenge Area
+// name
+// age
+// location
+// then generate a string person is 27 and lives in a place
+// then increase age
+
+let person = {
+    Name: 'Sierra',
+    Age: 36,
+    location: 'Jacksonville'
+}
+
+console.log(`${person.Name} is ${person.Age} years old, and lives in ${person.location}.`)
+
+person.Age = 37
+console.log(`${person.Name} is ${person.Age} years old, and lives in ${person.location}.`)
