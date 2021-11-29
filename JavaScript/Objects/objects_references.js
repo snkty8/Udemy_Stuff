9f82f3f82735048730c963a5b16c699cbc22f10c
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-29 15:25:19 -0500

    objects 7

---
+++ JavaScript/Objects/objects_references.js
@@ -0,0 +1,14 @@
+let myAccount = {
+    Name: 'Sierra',
+    expenses: 0,
+    income: 0
+}
+
+let addExpense = function (account, amount) {
+    //account.expenses = account.expenses + amount
+    account = {}
+    console.log(account)
+}
+
+addExpense(myAccount, 2.50)
+console.log(myAccount)
