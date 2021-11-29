fb4c17dbb6b0376bcc415e0a39f135b781b6c5e3
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-29 16:22:30 -0500

    objects 12

--- JavaScript/Objects/objects_methods.js
+++ JavaScript/Objects/objects_methods.js
@@ -1,12 +1,30 @@
 let resturant = {
     Name: 'Wok N Roll',
     guestCapacity: 75,
-    guestCount: 73,
+    guestCount: 0,
     checkAvalibility: function (partySize) {
         let seatsLeft = this.guestCapacity - this.guestCount
         return partySize <= seatsLeft
+    },
+    seatParty: function (partySize) {
+        this.guestCount = partySize + this.guestCount
+    },
+    removeParty: function (partySize) {
+        this.guestCount = this.guestCount - partySize
     }
+    
 }
 
-let Status = resturant.checkAvalibility(4)
-console.log(Status)
+//seatParty
+
+
+//removeParty
+
+
+
+resturant.seatParty(8)
+console.log(resturant.checkAvalibility)
+resturant.removeParty(8)
+console.log(resturant.checkAvalibility)
+// let Status = resturant.checkAvalibility(4)
+// console.log(Status)
