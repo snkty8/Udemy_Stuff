c6b50aac3c1192b418dbd10cc87b45ee2e983701
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-29 17:04:49 -0500

    objects 15

---
+++ JavaScript/Objects/number_method.js
@@ -0,0 +1,13 @@
+let num = 7.17
+
+console.log(num.toFixed())
+
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+
+let min = 10
+let max = 20
+let randomNum = Math.floor(Math.random() * (max - min + 1))
+console.log(randomNum)
+
