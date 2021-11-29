2095a73b81d688a8fa43d884e776aaf211df05e4
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-29 15:45:28 -0500

    objects 8

--- JavaScript/Objects/objects_references.js
+++ JavaScript/Objects/objects_references.js
@@ -5,10 +5,35 @@ let myAccount = {
 }
 
 let addExpense = function (account, amount) {
-    //account.expenses = account.expenses + amount
-    account = {}
-    console.log(account)
+    account.expenses = account.expenses + amount
 }
 
 addExpense(myAccount, 2.50)
+console.log(myAccount)
+
+
+// addIncome 
+let addIncome = function (account, amount) {
+    account.income = account.income + amount
+}
+// resetAccount
+let resetAccount = function (account) {
+    account.expenses = 0
+    account.income = 0
+}
+
+// getAccountSummary
+let getAccountSummary = function (account, expense, income) {
+    return`Account for ${myAccount.Name} has ${myAccount.income}. ${myAccount.expenses} in expenses.`
+}
+
+// Account for Sierra has $900. $1000 in come. $100 in expense.
+
+resetAccount(myAccount)
+console.log(myAccount)
+
+addIncome(myAccount, 100)
+console.log(myAccount)
+
+getAccountSummary(myAccount, 7, 8)
 console.log(myAccount)
