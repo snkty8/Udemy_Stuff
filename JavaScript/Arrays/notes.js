ba53beed25de43901ed8c4d8c23f9c45ef5dfabb
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-30 20:31:37 -0500

    array 4

--- JavaScript/Arrays/notes.js
+++ JavaScript/Arrays/notes.js
@@ -4,5 +4,25 @@ const notes = [
     'Note 3'
 ]
 
+//Pop: remove from the array
+// console.log(notes.pop())
+
+// // //Push: add to the array
+// notes.push('my new note')
+
+// //Shift: remove 1st item
+// console.log(notes.shift())
+
+// //Unshift: add to the beginning
+// notes.unshift('my first note')
+
+//splice: for below: start at index 1 and remove 1 item
+// notes.splice(1, 1)
+// add an item any where in the list
+// notes.splice(1, 1, 'This is the new second item')
+
+//Can use indexing to replace an item
+notes[2] = 'this is the new note 3'
+
 console.log(notes.length)
-console.log(notes[0])
+console.log(notes)
--- JavaScript/Arrays/to_do.js
+++ JavaScript/Arrays/to_do.js
@@ -1,8 +1,22 @@
 // Create an array with five todos
 const todos = ['Wake up', 'Brush teeth', 'Take a shower', 'Put on shoes', 'Go to work']
+
+//Delete 3rd item
+todos.splice(2, 1)
+console.log(todos)
+
+//Add a new item to the end
+todos.push('Eat lunch')
+console.log(todos)
+
+//remove the first item from the list
+todos.shift()
+console.log(todos)
+
+
 // You have x todos
-console.log('You have ' + todos.length + ' things to do today.')
-console.log(`You have ${todos.length} todos!!`)
+// console.log('You have ' + todos.length + ' things to do today.')
+// console.log(`You have ${todos.length} todos!!`)
 // Print 1 and second to last item
-console.log('To do: ' + todos[3] + ' and ' + todos[4])
-console.log(`To do: ${todos[todos.length - 2]} and ${todos[todos.length - 1]} `)
+// console.log('To do: ' + todos[3] + ' and ' + todos[4])
+// console.log(`To do: ${todos[todos.length - 2]} and ${todos[todos.length - 1]} `)
