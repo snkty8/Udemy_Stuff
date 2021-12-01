1c70b136596d062b3ff506b11160a7a1354160b3
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-11-30 20:10:09 -0500

    array 2

---
+++ JavaScript/Arrays/to_do.js
@@ -0,0 +1,6 @@
+// Create an array with five todos
+const todos = ['Wake up', 'Brush teeth', 'Take a shower', 'Put on shoes', 'Go to work']
+// You have x todos
+console.log(todos.length)
+// Print 1 and second to last item
+console.log('To do: ' + todos[3] + ' and ' + todos[4])
