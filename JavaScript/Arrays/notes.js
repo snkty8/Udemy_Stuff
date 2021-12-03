5795b50f2b36117f8e9473633232477c391c56b0
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-12-03 15:31:09 -0500

    array 9

--- JavaScript/Arrays/notes.js
+++ JavaScript/Arrays/notes.js
@@ -1,4 +1,4 @@
-const notes = [{}, {
+const notes = [{
     title: 'My next trip',
     body: 'I would like to go to Spain'
 }, {
@@ -9,49 +9,19 @@ const notes = [{}, {
     body: 'Get a new seat'
 }]
 
-//Pop: remove from the array
-// console.log(notes.pop())
 
-// // //Push: add to the array
-// notes.push('my new note')
+const findNote = function (notes, noteTitle) {
+    return notes.find(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
 
-// //Shift: remove 1st item
-// console.log(notes.shift())
-
-// //Unshift: add to the beginning
-// notes.unshift('my first note')
-
-//splice: for below: start at index 1 and remove 1 item
-// notes.splice(1, 1)
-// add an item any where in the list
-// notes.splice(1, 1, 'This is the new second item')
-
-//Can use indexing to replace an item
-// notes[2] = 'this is the new note 3'
-
-//For Each
-// Passing a function in a functuin is called a call back function
-// notes.forEach(function (item, index) {
-//     console.log(index)
-//     console.log(item)
-// })
-
-console.log(notes.length)
-console.log(notes)
-
-// // Counting ...1
-// //set count variable, then add condition, check the count
-// for (let count = 2; count >= 0; count --) {
-//     console.log(count)
+// const findNote = function (notes, noteTitle) {
+//     const index = notes.findIndex(function (note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
 // }
+const note = findNote(notes, 'Office modification')
+console.log(note)
 
-// for (let count = notes.length - 1; count >= 0; count--) {
-//     console.log(notes[count])
-// }
-
-const index = notes.findIndex(function (note, index) {
-    console.log(note)
-    return note.title === 'Habits to work on'
-})
-
-console.log(index)
--- JavaScript/Arrays/to_do.js
+++ JavaScript/Arrays/to_do.js
@@ -1,36 +1,28 @@
 // Create an array with five todos
-const todos = ['Wake up', 'Brush teeth', 'Take a shower', 'Put on shoes', 'Go to work']
+const todos = [{
+    text: 'Wake up',
+    completed: true
+}, {
+    text: 'Brush teeth',
+    completed: false
+}, {
+    text: 'Take a shower',
+    completed: false
+}, {
+    text: 'Put on shoes',
+    completed: false
+}, {
+    text: 'Go to work',
+    completed: true
+}]
+const deleteTodo = function (todos, todoText) {
+    const index = todos.findIndex(function (todo) {
+        return todo.text.toLowerCase() === todoText.toLowerCase()
+    })
+    if (index > -1) {
+        todos.splice(index, 1)
+    }
+}
 
-// //Delete 3rd item
-// todos.splice(2, 1)
-// console.log(todos)
-
-// //Add a new item to the end
-// todos.push('Eat lunch')
-// console.log(todos)
-
-// //remove the first item from the list
-// todos.shift()
-// console.log(todos)
-
-
-// You have x todos
-// console.log('You have ' + todos.length + ' things to do today.')
-console.log(`You have ${todos.length} todos!!`)
-// Print 1 and second to last item
-// console.log('To do: ' + todos[3] + ' and ' + todos[4])
-// console.log(`To do: ${todos[todos.length - 2]} and ${todos[todos.length - 1]} `)
-
-// 1. stuff
-// 2. more stuff
-
-// Passing a function in a functuin is called a call back function
-todos.forEach(function (item, index) {
-    const num = index + 1
-    console.log(`${num}. ${item}`)
-})
-
-// for (let count = 0; count < todos.length; count++) {
-//     const num = count + 1
-//     console.log(`${num}. ${todos[count]}`)
-// }
+deleteTodo(todos, 'Take a shower')
+console.log(todos)
