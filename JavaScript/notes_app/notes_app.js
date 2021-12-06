3e2543664a7e6e5b238b6a8bdcae17cdfeb1717d
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-12-06 17:16:35 -0500

    browser 3

--- JavaScript/notes_app/notes_app.js
+++ JavaScript/notes_app/notes_app.js
@@ -1,4 +1,15 @@
 // DOM - Document Object Model
 
-const p = document.querySelector('p')
-console.log(p)
+
+// Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// Qurey all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    p.textContent = '*****'
+    // console.log(p.textContent)
+    // p.remove()
+})
