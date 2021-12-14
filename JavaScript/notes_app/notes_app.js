6bc567dc7eb38dc7128fe01d7005456a161b1fd5
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-12-13 20:36:46 -0500

    dom

--- JavaScript/notes_app/notes_app.js
+++ JavaScript/notes_app/notes_app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
 // DOM - Document Object Model
 
 
@@ -13,3 +24,8 @@ ps.forEach(function (p) {
     // console.log(p.textContent)
     // p.remove()
 })
+
+// Add a new element
+const newParagrah = document.createElement('p')
+newParagrah.textContent = 'This a new element from JavaScript'
+document.querySelector('body').appendChild(newParagrah)
--- JavaScript/todo_app/todos.js
+++ JavaScript/todo_app/todos.js
@@ -1,8 +1,32 @@
-const the = document.querySelectorAll('p')
+const todos = [{
+    text: 'Wake up',
+    completed: true
+}, {
+    text: 'Brush teeth',
+    completed: false
+}, {
+    text: 'Take a shower',
+    completed: false
+}, {
+    text: 'Put on shoes',
+    completed: false
+}, {
+    text: 'Go to work',
+    completed: true
+}]
 
-the.forEach(function (p) {
-    if (p.textContent.includes('the')) {
-        p.remove()
-    } 
+// const the = document.querySelectorAll('p')
 
-})
+// the.forEach(function (p) {
+//     if (p.textContent.includes('the')) {
+//         p.remove()
+//     } 
+
+// })
+
+// You have 2 todos left (p element)
+// Add a paragrah for each todo above (use text value)
+
+const newParagrah = document.createElement('p')
+newParagrah.textContent = 'You still have 2 todos for the day'
+document.querySelector('body').appendChild(newParagrah)
