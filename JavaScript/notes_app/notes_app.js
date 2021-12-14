5f8fabdaa35284b49b70c021caa8d86393cbfa2b
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-12-14 17:18:49 -0500

    dom 6

--- JavaScript/notes_app/notes_app.js
+++ JavaScript/notes_app/notes_app.js
@@ -9,6 +9,27 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filterNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filterNotes.forEach(function (note) {
+        const noteElement = document.createElement('p')
+        noteElement.textContent = note.title
+        document.querySelector('#notes').appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
 // # grabs id
 document.querySelector("#create-note").addEventListener("click", function (e) {
     console.log('Did this work?')
@@ -27,5 +48,6 @@ document.querySelector('#remove-all').addEventListener('click', function  () {
 })
 
 document.querySelector("#search-text").addEventListener("input", function (e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
