5a64f6f77948885714c0721b62f7a32ffeefc8d6
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-12-14 18:45:44 -0500

    dom 13

--- JavaScript/notes_app/notes_app.js
+++ JavaScript/notes_app/notes_app.js
@@ -46,6 +46,6 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
     renderNotes(notes, filters)
 })
 
-document.querySelector('#for-fun').addEventListener('change', function (e) {
-    console.log(e.target.checked)
+document.querySelector("#filter-by").addEventListener("change", function (e){
+    console.log(e.target.value)
 })
