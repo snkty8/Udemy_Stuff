ba4774421f74b2359968c2d62806bbeb810c1d23
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-12-14 17:01:54 -0500

    dom 5

--- JavaScript/todo_app/todos.js
+++ JavaScript/todo_app/todos.js
@@ -42,7 +42,12 @@ todos.forEach(function (todo) {
     document.querySelector('body').appendChild(p)
 })
 
-
+// Listen for new todo
 document.querySelector("#new-todo").addEventListener("click", function (e) {
     console.log('Let us see!!')
+})
+
+// Listen for todo text change
+document.querySelector("#add-todos").addEventListener("input", function (e) {
+    console.log(e.target.value)
 })
