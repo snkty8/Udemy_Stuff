95f55045dc943d40ce5f67e1742da72f5cfdbc47
snkty8 <78936833+snkty8@users.noreply.github.com>
2022-01-12 19:48:01 -0500

    complexte DOM 1

--- JavaScript/notes_app/notes_functions.js
+++ JavaScript/notes_app/notes_functions.js
@@ -16,12 +16,21 @@ const saveNotes = function (notes) {
 
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
-    const noteElement = document.createElement('p')
+    const noteElement = document.createElement('div')
+    const textElement = document.createElement('span')
+    const button = document.createElement('button')
 
+    // Setup the remove note button
+    button.textContent = 'x'
+    noteElement.appendChild(button)
+
+        // Setup the note title text
         if (note.title.length > 0) {
-            noteElement.textContent = note.title
+            textElement.textContent = note.title
         } else { 
-            noteElement.textContent = "Unnamed note"
+            textElement.textContent = "Unnamed note"
+
+            noteElement.appendChild(textElement)
         }
         return noteElement
 }
