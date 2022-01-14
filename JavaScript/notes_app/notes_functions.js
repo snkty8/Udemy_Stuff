351df8250eccd07a98911b21b0d50a4d4b68fa5a
snkty8 <78936833+snkty8@users.noreply.github.com>
2022-01-13 19:20:55 -0500

    uuidv4 1

--- JavaScript/notes_app/notes_functions.js
+++ JavaScript/notes_app/notes_functions.js
@@ -14,6 +14,17 @@ const saveNotes = function (notes) {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
+// Remove a note from the list
+const removeNote = function (id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id === id
+    })
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+}
+
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
     const noteElement = document.createElement('div')
@@ -23,6 +34,11 @@ const generateNoteDOM = function (note) {
     // Setup the remove note button
     button.textContent = 'x'
     noteElement.appendChild(button)
+    button.addEventListener('click', function () {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
 
         // Setup the note title text
         if (note.title.length > 0) {
