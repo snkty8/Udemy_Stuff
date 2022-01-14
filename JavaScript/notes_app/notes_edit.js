ce0525f28936930f95d8d313e1a5a1a7da3c2d02
snkty8 <78936833+snkty8@users.noreply.github.com>
2022-01-14 16:15:26 -0500

    uuidv4 8

--- JavaScript/notes_app/notes_edit.js
+++ JavaScript/notes_app/notes_edit.js
@@ -1,9 +1,33 @@
-const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
+const noteId = location.hash.substring(1)
 const note = notes.find(function (note) {
     return note.id === noteId
 })
 
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
+
 if (note === undefined) {
     location.assign('/notes_app/index.html')
 }
+
+titleElement.value = note.title
+bodyElement.value = note.body
+
+titleElement.addEventListener('input', function (e) {
+    note.title = e.target.value
+    saveNotes(notes)
+})
+
+bodyElement.addEventListener('input', function (e) {
+    note.body = e.target.value
+    saveNotes(notes)
+})
+
+removeElement.addEventListener('click', function (e) {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/notes_app/index.html')
+})
+
