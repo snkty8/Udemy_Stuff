d064d76f8d7e23593173786f11ac07a5c52a827f
snkty8 <78936833+snkty8@users.noreply.github.com>
2022-01-24 20:27:31 -0500

    moment 1

--- JavaScript/notes_app/notes_app.js
+++ JavaScript/notes_app/notes_app.js
@@ -36,30 +36,7 @@ window.addEventListener('storage', function (e) {
     }
 })
 
-// Unix Epoch - Janurary 1st 1970 00:00:00
-
-const now = new Date()
-const timeStamp = now.getTime()
-
-const myDate = new Date(timeStamp)
-console.log(myDate.getFullYear())
-
-// console.log(`Year: ${now.getFullYear()}`)
-// console.log(`Month: ${now.getMonth()}`)
-// console.log(`Day of Month: ${now.getDate()}`)
-// console.log(`Hour: ${now.getHours()}`)
-// console.log(`Minute: ${now.getMinutes()}`)
-// console.log(`Seconds: ${now.getSeconds()}`)
-
-
-const dateOne = new Date('March 1 2018 12:00:00')
-const dateTwo = new Date()
-
-const dateOneTimeStamp = dateOne.getTime()
-const dateTwoTimeStamp = dateTwo.getTime()
-
-if (dateOneTimeStamp > dateTwoTimeStamp) {
-    console.log(dateOne.toString())
-} else if (dateTwoTimeStamp > dateOneTimeStamp) {
-    console.log(dateTwo.toString())
-}
+const now = moment()
+now.add(1, 'year').subtract(20, 'days')
+console.log(now.format('MMMM Do YYYY'))
+console.log(now.fromNow())
