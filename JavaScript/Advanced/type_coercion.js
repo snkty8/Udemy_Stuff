6addffb02b798c6664e6b52a53dbc0a20a546461
snkty8 <78936833+snkty8@users.noreply.github.com>
2022-01-27 19:58:40 -0500

    type error

---
+++ JavaScript/Advanced/type_coercion.js
@@ -0,0 +1,6 @@
+// Type coercion: a string, a number, and a boolean
+
+const value = true + 12
+console(value)
+const type = typeof 103
+console.log(type)
--- JavaScript/Functions/grade_calc.js
+++ JavaScript/Functions/grade_calc.js
@@ -4,22 +4,35 @@
 
 let grade_cal = function (score, possibleScore) {
 
-    let percent = (score/possibleScore)*100
+    if (typeof score === 'number' && typeof possibleScore === 'number') {
+
+        const percent = (score/possibleScore)*100
+
     
-    if (percent >= 90) {
-        letterGrade = 'A'
-    } else if (percent <=89 && percent >= 80) {
-        letterGrade = 'B'
-    } else if (percent <= 79 && percent>=70) {
-        letterGrade = 'C'
-    } else if (percent <= 69 && percent >= 60) {
-        letterGrade = 'D'
+        if (percent >= 90) {
+            letterGrade = 'A'
+        } else if (percent <=89 && percent >= 80) {
+            letterGrade = 'B'
+        } else if (percent <= 79 && percent>=70) {
+            letterGrade = 'C'
+        } else if (percent <= 69 && percent >= 60) {
+            letterGrade = 'D'
+        } else {
+            letterGrade = 'F'
+        }
+
+        return `Your score is ${score} out of a possible ${possibleScore}. That is ${percent}% You got a ${letterGrade}!!`
+ 
     } else {
-        letterGrade = 'F'
-    }
+        throw Error ('Arguement must be a number')
+    }    
     
-    return `Your score is ${score} out of a possible ${possibleScore}. That is ${percent}% You got a ${letterGrade}!!`
 }
 
-let grade = grade_cal(75, 100)
-console.log(grade)
+try {
+    let grade = grade_cal(95, 100)
+    console.log(grade)
+} catch (e) {
+    console.log(e.message)
+}
+
